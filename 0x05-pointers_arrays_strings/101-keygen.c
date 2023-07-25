a2a2367561b6754b138b319a303201a931133c27
Ahmed ElSamman <ahmed-alsamman@hotmail.com>
2023-07-25 01:13:32 -0700

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -11,20 +11,22 @@ return validChars[randomIndex];
 }
 /*Function to generate a random password of a given length*/
 void generatePassword(int length) {
-for (int i = 0; i < length; i++) {
+int i;
+for (i = 0; i < length; i++) {
 char randomChar = getRandomCharacter();
 putchar(randomChar);
 }
 putchar('\n');
 }
 int main(void) {
+int j;
 /*Number of passwords to generate*/
 int numPasswords = 10;
 /*Length of each password*/
 int passwordLength = 8;
 /*Seed the random number generator using the current time*/ 
 srand(time(0));
-for (int i = 0; i < numPasswords; i++) {
+for (j = 0; j < numPasswords; j++) {
 generatePassword(passwordLength);
 }
 return 0;
