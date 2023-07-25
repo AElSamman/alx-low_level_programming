8c1b58ff023f6f9e93f559a46f4da660ce2f919d
Ahmed ElSamman <ahmed-alsamman@hotmail.com>
2023-07-25 01:02:38 -0700

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,34 +7,16 @@
  */
 void puts2(char *str)
 {
-/**Declare and initialize an integer variable i with the value 0*/
-int i = 0; /**This will be used as an index to traverse the string*/
-/** Start of the function implementation.*/
-/** Check if the input string str is NULL.*/
+int i = 0;
 if (str == NULL)
-/**If it is, return early from the function*/
 return;
-/**
-* Start of a while loop that will execute as long as the character
-* at index i of the string str is not the null
-* @'\0': null terminator 
-*/
 while (str[i] != '\0')
 {
-/**
-* Print the character if it has an even index
-*/
 if (i % 2 == 0)
 putchar(str[i]);
 i++;
-/**
-* Handle long strings that exceed the index range of int
-*/
 if (i == 0)
 break;
 }
-/**After the loop finishes, print a newline character '\n'*/
-/**to move to the next line and create a new line in the output*/
-/** @'\n': newline character*/
 putchar('\n');
 }
