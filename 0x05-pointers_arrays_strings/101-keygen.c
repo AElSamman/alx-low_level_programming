6f256716bf9030e1401cedc54df56a9795ace425
Ahmed ElSamman <ahmed-alsamman@hotmail.com>
2023-07-24 23:16:29 -0700

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -40,6 +40,7 @@ return password;
 int main(void)
 {
 char *password = generate_password();
-printf("Tada! Congrats ","Generated Password: %s\n", password);
+printf("Tada! Congrats");
+printf("Generated Password: %s\n", password);
 return 0;
 }
