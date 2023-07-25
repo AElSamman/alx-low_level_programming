7f4e8ccc363226a61c75787cffd94d9d62a00aa4
Ahmed ElSamman <ahmed-alsamman@hotmail.com>
2023-07-25 00:06:00 -0700

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -3,12 +3,27 @@
  * puts2 - Prints every other character of a string, starting with the first character.
  *
  * @str: The input string.
+ *
+ * puts2 - Prints every other character of a string, starting with the first character.
+ * 
+ * Return: void
  */
 void puts2(char *str)
 {
+/**
+ * Declare and initialize an integer variable i with the value 0.
+ * This will be used as an index to traverse the string
+ */
 int i = 0;
+/**
+ * Start of the function implementation. Check if the input string str is NULL.
+ * If it is, return early from the function
+ */
 if (str == NULL)
 return;
+/**Start of a while loop that will execute as long as the character
+* at index i of the string str is not the null terminator '\0'
+*/
 while (str[i] != '\0')
 {
 /**
