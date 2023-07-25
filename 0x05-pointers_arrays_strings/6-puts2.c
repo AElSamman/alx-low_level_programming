ec825a739941ef908ce482062101828ec1596387
Ahmed ElSamman <ahmed-alsamman@hotmail.com>
2023-07-24 23:48:07 -0700

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,18 +1,27 @@
 #include <stdio.h>
 /**
- * puts2 - Prints every other character of a string,
- *  starting with the first character.
- * @str: Pointer to the string.
+ * puts2 - Prints every other character of a string, starting with the first character.
  *
- * Return: void
+ * @str: The input string.
  */
 void puts2(char *str)
 {
+if (str == NULL)
+return;
 int i = 0;
 while (str[i] != '\0')
 {
+/**
+*  Print the character if it has an even index
+*/ 
+if (i % 2 == 0)
 putchar(str[i]);
-i += 2;
+i++;
+/**
+* Handle long strings that exceed the index range of int
+*/
+if (i == 0)
+break;
 }
 putchar('\n');
 }
