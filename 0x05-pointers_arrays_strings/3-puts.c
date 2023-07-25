ec64c997421421be0a32e2437f7dd30f404cc7e5
Ahmed ElSamman <ahmed-alsamman@hotmail.com>
2023-07-24 19:57:59 -0700

    3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -8,16 +8,10 @@
  */
 void _puts(char *str)
 {
-	int i = 0;
-
-	/* Loop until the null terminator is reached */
-	while (str[i] != '\0')
-	{
-		/* Write each character to the standard output */
-		write(1, &str[i], 1);
-		i++;
-	}
-
-	/* Write a new line character to the standard output */
-	write(1, "\n", 1);
+    while (*str != '\0')
+    {
+        putchar(*str);
+        str++;
+    }
+    putchar('\n');
 }
