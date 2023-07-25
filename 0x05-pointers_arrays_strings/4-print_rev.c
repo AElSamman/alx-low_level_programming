91cd061090b74a1aff3db3c357c32a09e0e2cb24
Ahmed ElSamman <ahmed-alsamman@hotmail.com>
2023-07-24 20:15:37 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,6 +1,41 @@
 #include <stdio.h>
 
 /**
+ * _strlen - Returns the length of a string.
+ * @s: Pointer to the string.
+ *
+ * Return: The length of the string.
+ */
+int _strlen(char *s)
+{
+    int length = 0;
+    while (s[length] != '\0')
+    {
+        length++;
+    }
+    return length;
+}
+
+/**
+ * print_rev_recursive - Helper function to print a string in reverse recursively.
+ * @s: Pointer to the string to be printed in reverse.
+ * @length: Length of the string.
+ *
+ * Return: void
+ */
+void print_rev_recursive(char *s, int length)
+{
+    if (length == 0)
+    {
+        putchar('\n');
+        return;
+    }
+
+    putchar(s[length - 1]);
+    print_rev_recursive(s, length - 1);
+}
+
+/**
  * print_rev - Prints a string in reverse followed by a new line.
  * @s: Pointer to the string to be printed in reverse.
  *
@@ -8,20 +43,6 @@
  */
 void print_rev(char *s)
 {
-    int length = 0;
-
-    /* Calculate the length of the string */
-    while (s[length] != '\0')
-    {
-        length++;
-    }
-
-    /* Print the string in reverse */
-    for (int i = length - 1; i >= 0; i--)
-    {
-        putchar(s[i]);
-    }
-
-    /* Write a new line character to the standard output */
-    putchar('\n');
+    int length = _strlen(s);
+    print_rev_recursive(s, length);
 }
